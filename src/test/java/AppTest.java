7dbf76885197a12ab5f8947f02328639a477e391
Omkar <omkarsalunke1978@gmail.com>
2025-02-18 11:56:05 +0530

    added files

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    void testApp() {
+        assertEquals(2, 1 + 1, "Basic math test");
+    }
+}
